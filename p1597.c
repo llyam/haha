a3883d3ba092f62dca64f7ea3e37a4ec73f2f7de
llyam <42264551+llyam@users.noreply.github.com>
2018-11-18 21:18:45 +0800

    Add files via upload

--- p1597.c
+++ p1597.c
@@ -2,28 +2,33 @@
 	> File Name:p1597.c
 	> Author: shenyuchi
 	> Mail: 1349334667@qq.com
-	> Created Time:2018年09月21日 星期五 16时11分30秒
+	> Created Time:2018年11月16日 星期五 20时23分50秒
  ************************************************************************/
 
 #include <stdio.h>
 #include <string.h>
-
 int main() {
-    char ch[15];
-    char ss['a'] = ss['b'] = ss['c'] = '0';
-    scanf("%s", &ch);
-    int n = strlen(ch);
-    for (int i = 0, j = 3; i < n; i += 5, j += 5) {
-        if (ch[i] == 'a') {
-            ss['a'] = ch[j];
+    char str['c' + 1];
+    str['c'] = '0', str['b'] = '0', str['a'] = '0';
+    char name[100], p;
+    scanf("%s", name);
+    int n = strlen(name);
+    for (int i = 0; i < n; i++) {
+        if (name[i] == 'a' || name[i] == 'b' || name[i] == 'c') {
+            p = name[i];
         }
-        if (ch[i] == 'b') {
-            ss['b'] = ch[j];
-        }
-        if (ch[i] == 'c') {
-            ss['c'] = ch[j];
+        if (name[i] == '=' && i + 1 < n) {
+            str[p] = name[i + 1];
         }
     }
-    printf("%c %c %c", ss['a'], ss['b'], ss['c']);
+    for (int i = 'a'; i <= 'c'; i++) {
+        if (str[i] == '0') {
+            printf("0");
+        } else {
+        printf("%c", str[i]);
+        }
+        if(i != 'c' || str[i] == '0')  printf(" ");
+    }
+    printf("\n");
     return 0;
 }
